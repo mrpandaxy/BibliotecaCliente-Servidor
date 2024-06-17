6ee76b30ff18c1de7d75c7b8de632b3a63eecaa4
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2024-06-17 20:10:33 -0300

    Criacao da funcao carregar livros

--- Biblioteca/src/main/java/Biblioteca.java
+++ Biblioteca/src/main/java/Biblioteca.java
@@ -1,5 +1,10 @@
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Biblioteca {
@@ -9,6 +14,20 @@ public class Biblioteca {
 
     public Biblioteca() throws IOException {
         objectMapper = new ObjectMapper();
+        carregarLivros();
+    }
 
+    private void carregarLivros() throws IOException {
+        File file = new File(json);
+        if (file.exists()) {
+            System.out.println("Carregando livros...");
+            JsonNode rootNode = objectMapper.readTree(file);
+            JsonNode livrosNode = rootNode.path("livros");
+            livros = objectMapper.convertValue(livrosNode, new TypeReference<List<Livros>>() {});
+            System.out.println("Livros carregados com sucesso!");
+        } else {
+            System.out.println("Não foi possível carregar livros");
+            livros = new ArrayList<>();
+        }
     }
 }
