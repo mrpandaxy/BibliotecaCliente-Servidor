76c376cb22a23deab45cfd187575bc93c2eb7a00
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2024-06-17 20:09:36 -0300

    Criacao dos atributos e construtor da classe Biblioteca

--- Biblioteca/src/main/java/Biblioteca.java
+++ Biblioteca/src/main/java/Biblioteca.java
@@ -1,2 +1,14 @@
+import java.io.IOException;
+import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Biblioteca {
+    private List<Livros> livros;
+    private static final String json = "livros.json";
+    private ObjectMapper objectMapper;
+
+    public Biblioteca() throws IOException {
+        objectMapper = new ObjectMapper();
+
+    }
 }
